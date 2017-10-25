d12ea7d606ff366e55237b90eb6eac304bead775
xdass <420600@gmail.com>
2017-10-25 23:46:34 +0500

    Init commit

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 [TODO. There will be project description]
 
-# Project Goals
+# How to run program
+Script needs for work, Python version not less than 3.4
 
+Run script on Linux:
+
+$ python lang_frequency.py text.txt
+
+Run script on Windows is the same as Linux.
+# Project Goals
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,21 @@
+from collections import Counter
+import re
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, encoding='utf-8') as file:
+        file_data = file.read()
+    return file_data
 
 
-def get_most_frequent_words(text):
-    pass
+def get_most_frequent_words(text, words_count=10):
+    words_pattern = re.compile(r'[^\W_+]+')
+    separated_words = re.findall(words_pattern, text)
+    return Counter(list(word for word in separated_words)).most_common(words_count)
 
 
 if __name__ == '__main__':
-    pass
+    text = load_data('text.txt')
+    common_words = get_most_frequent_words(text)
+    for common_word, count in common_words:
+        print('Слово "{}" в тексте встречается {} раз{}'.format(common_word, count, 'а' if count % 2 == 0 else ''))
