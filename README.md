634c78fbd81fd2e7179215c9e494500184cef7d9
xdass <420600@gmail.com>
2017-10-26 12:35:14 +0500

    Refactoring code for best readability

--- README.md
+++ README.md
@@ -1,13 +1,25 @@
 # Frequency Analysis of Words
-
-[TODO. There will be project description]
-
+It's a simple program that analyse word frequency in text and print the ten most common.
 # How to run program
 Script needs for work, Python version not less than 3.4
 
 Run script on Linux:
 
+```
 $ python lang_frequency.py text.txt
+
+Слово "где" в тексте встречается 4 раза
+Слово "в" в тексте встречается 4 раза
+Слово "и" в тексте встречается 2 раза
+Слово "восемьдесят" в тексте встречается 2 раза
+Слово "четвертом" в тексте встречается 2 раза
+Слово "тысяча" в тексте встречается 2 раза
+Слово "плетут" в тексте встречается 1 раз
+Слово "столько" в тексте встречается 1 раз
+Слово "страну" в тексте встречается 1 раз
+Слово "козы" в тексте встречается 1 раз
+
+```
 
 Run script on Windows is the same as Linux.
 # Project Goals
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,21 +1,31 @@
 from collections import Counter
+import sys
 import re
 
 
 def load_data(filepath):
-    with open(filepath, encoding='utf-8') as file:
-        file_data = file.read()
-    return file_data
+    try:
+        with open(filepath, encoding='utf-8') as file:
+            file_data = file.read()
+        return file_data
+    except FileNotFoundError:
+        return None
 
 
-def get_most_frequent_words(text, words_count=10):
+def get_most_frequent_words(raw_text, words_count=10):
     words_pattern = re.compile(r'[^\W_+]+')
-    separated_words = re.findall(words_pattern, text)
+    separated_words = re.findall(words_pattern, raw_text)
     return Counter(list(word for word in separated_words)).most_common(words_count)
 
 
 if __name__ == '__main__':
-    text = load_data('text.txt')
-    common_words = get_most_frequent_words(text)
-    for common_word, count in common_words:
-        print('Слово "{}" в тексте встречается {} раз{}'.format(common_word, count, 'а' if count % 2 == 0 else ''))
+    if len(sys.argv) > 1:
+        text = load_data(sys.argv[1])
+        if text is not None:
+            common_words = get_most_frequent_words(text)
+            for common_word, count in common_words:
+                print('Слово "{}" в тексте встречается {} раз{}'.format(common_word, count, 'а' if count % 2 == 0 else ''))
+        else:
+            print('Файл не найден')
+    else:
+        print('Укажите имя файла: python lang_frequency.py <filename>')
